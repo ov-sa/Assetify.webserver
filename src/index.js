1064460e6636c481069470fb3fb3e5c4dcacec96
Tron <89867985+Aviril@users.noreply.github.com>
2024-03-06 00:16:41 +0300

    Added optional port support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-module.exports = () => {
+module.exports = (config) => {
+    config = vKit.isObject(config) ? config : {}
     (async () => {
         require("@vstudio/vital.network")
         vKit.print("\x1b[33m━ Assetify (Server) | Launching...")
         let Assetify = vNetwork.create({
-            port: 33022,
+            port: vKit.Number(config.port) || 33022,
             isCaseSensitive: true
         })
         if (!await Assetify.connect()) return vKit.print("\x1b[33m━ Assetify (Server) | \x1b[31mFailed to launch due to technical issues...\x1b[31m")
